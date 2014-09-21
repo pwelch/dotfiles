b41f6f48a1b2e10afab2c569fc449bdebb049e34
pwelch <paul@pwelch.net>
2014-09-21 11:27:22 -0400

    Add ZSH Aliases
    - Move ZSH Aliases
    - Add Additional Jrnls
    
    Add ZSH Aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,8 @@
-alias reload!='. ~/.zshrc'
+# Add aliases
+alias vi='vim'
+alias reload!=". ~/.zshrc && echo 'ZSH config reloaded from ~/.zshrc'"
+alias notebook='cd ~/Code/notebook'
+
+# Stop zsh autocorrection
+alias knife='nocorrect knife'
+alias iex='nocorrect iex'
