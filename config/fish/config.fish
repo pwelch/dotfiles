5d23c038363460db7a3c1075ce65e37c997098c4
pwelch <git@welch.pw>
2021-05-11 17:40:34 -0400

    Update Starship & Fish configs

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,26 @@
 # ~/.config/fish/config.fish
 
+set --export SHELL /usr/bin/fish
+set --export EDITOR "vim"
+set -e fish_greeting
+
 # enable vi-mode bindings
 fish_vi_key_bindings
 
+# enable starship
 starship init fish | source
+
+# Change history prompt to show time
+function history
+    builtin history --show-time='%F %T '
+end
+
+# Define aliases
+alias vi='vim'
+alias be='bundle exec'
+alias cpwd='pwd | pbcopy'
+alias gst='git status'
+alias hist='history'
+
+# Add to PATH
+fish_add_path ~/bin
